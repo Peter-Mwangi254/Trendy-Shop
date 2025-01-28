7487efc7acdd45720a7c929160c6de370c361969
Peter-Mwangi254 <pm6093628@gmail.com>
2025-01-28 15:42:34 +0300

    Added changes to the components

---
+++ README.md
@@ -0,0 +1,80 @@
+# Smart-shop Website
+
+A Smart-shop is an Ecommerce Website made with React.js Framework.
+
+
+## Demo
+
+https://smart-shop-psi.vercel.app/ 
+
+## Features
+
+- Easy to integrate with Backend
+- Fully Responsive
+
+## Project Description
+The project Description can be found in my google [slide](https://docs.google.com/presentation/d/1D6xLbNq0l6w3QGvJrw7gWGuXnmxmtI0mvUoHBokyatY/edit?usp=sharin)
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/X4jrq9j/t-shirt-wallpaper.png)
+![smart-shop-index-1](https://i.ibb.co/fp350Jb/smart-shop-index-1.png)
+![smart-shop-product](https://i.ibb.co/Fm840my/12.png)
+![Electronics-Samsung-19inch](https://i.ibb.co/Gk8gYgk/13.png)
+![Customer-billing-address](https://i.ibb.co/F02Stc5/004.png)
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/4mubarak/Smart-shop.git
+```
+
+Go to the project directory
+
+```bash
+  cd Smart-shop
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+
+
+## Tech Stack
+
+* [React](https://reactjs.org/)
+* [Redux](https://redux.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Fake Store API](https://fakestoreapi.com/)
+
+## Contributing
+
+Contributions are always welcome!
+Just raise an issue, we will discuss it.
+
+
+## Feedback
+
+If you have any feedback, please reach out to me [here](https://4mubarak.github.io)
+
+## Authors
+
+Mubarak Abdullahi
+* [GitHub](https://github.com/4mubarak/Smart-shop.git)
+* [LinkedIn](https://www.linkedin.com/in/mubarak-abdullahi-a30411161)
+* [Twitter](https://twitter.com/Mubar101)
+
+## License
+All Rights reserved
+
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          A young and ambitious web developer formerly lived in a thriving metropolis full of
+ambitions and goals.I was enthusiastic about establishing meaningful initiatives and was
+particularly interested in e-commerce. One day, while browsing several internet businesses,
+an inspiration struck me - a vision for an unusual ecommerce portfolio project.
+I was inspired when I spotted a market need for a platform that effortlessly combined
+technology with a personalized purchasing experience. The current e-commerce websites were
+functional, but they lacked flair and uniqueness. I envisioned a platform that not only
+enabled consumers to buy things, but also created a personalized and interesting trip for
+each shopper.
+The concept emerged when I considered combining cutting-edge technology to improve the user
+experience. The idea included virtual try-ons for apparel and accessories, tailored product
+recommendations based on user preferences, and an easy-to-use interface that felt more like
+a personal shopping assistant than a standard e-commerce website.
+The planning process begins with an in-depth examination of the target audience. The
+e-commerce portfolio project was more than simply an exhibition of technological abilities;
+it demonstrated creativity, originality, and a thorough awareness of customer demands. The
+platform provided a view into the future of online purchasing, with each user feeling seen,
+heard, and pleased by a tailored and engaging experience.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
